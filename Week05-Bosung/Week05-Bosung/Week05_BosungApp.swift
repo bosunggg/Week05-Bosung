7be396001a32a0182d7c7d9a64502d20272a54f4
Bosung <112652093+bosunggg@users.noreply.github.com>
2023-02-28 11:02:37 -0500

    added

---
+++ Week05-Bosung/Week05-Bosung/Week05_BosungApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week05_BosungApp.swift
+//  Week05-Bosung
+//
+//  Created by Bosung Kim on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Week05_BosungApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
